c4d7d94c3f8e37e97fac226a393762ba94333d34
petrgali <opelliek@gmail.com>
2021-05-15 15:15:25 +0600

    refactor: update task module

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,55 +1,24 @@
 const router = require('express').Router({ mergeParams: true });
-const Task = require('./task.model');
 const taskService = require('./task.service');
 
 router.route('/').get(async (req, res) => {
   const tasksList = await taskService.getBoardTasks(req.params.boardId);
-  res.status(tasksList ? 200 : 401).json(tasksList.map(Task.toResponse));
+  res.status(tasksList ? 200 : 401).json(tasksList);
 });
-
 router.route('/').post(async (req, res) => {
-  const task = new Task({
-    title: req.body.title,
-    order: req.body.order,
-    description: req.body.description,
-    userId: req.body.userId,
-    boardId: req.params.boardId,
-    columnId: req.body.columnId,
-  });
-  await taskService.addBoardTask(task);
-  res.status(201).json(Task.toResponse(task));
+  const task = await taskService.addBoardTask(req);
+  res.status(201).json(task);
 });
-
 router.route('/:taskId').get(async (req, res) => {
-  const task = await taskService.getTaskById({
-    board: req.params.boardId,
-    task: req.params.taskId,
-  });
+  const task = await taskService.getTaskById(req.params);
   res.status(task ? 200 : 404).json(task);
 });
-
 router.route('/:taskId').put(async (req, res) => {
-  const update = {
-    title: req.body.title,
-    order: req.body.order,
-    description: req.body.description,
-    userId: req.body.userId,
-    boardId: req.body.boardId,
-    columnId: req.body.columnId,
-  };
-  const params = {
-    board: req.params.boardId,
-    task: req.params.taskId,
-  };
-  await taskService.updateTask(params, update);
-  const task = await taskService.getTaskById(params);
+  const task = await taskService.updateTask(req.body, req.params);
   res.status(200).json(task);
 });
 router.route('/:taskId').delete(async (req, res) => {
-  await taskService.deleteTask({
-    board: req.params.boardId,
-    task: req.params.taskId,
-  });
+  await taskService.deleteTask(req.params);
   res.status(204).json({});
 });
 module.exports = router;
--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,10 +1,45 @@
 const taskRepo = require('./task.memory.repository');
+const Task = require('./task.model');
 
 const getBoardTasks = (idx) => taskRepo.getBoardTasks(idx);
-const addBoardTask = (task) => taskRepo.addBoardTask(task);
-const getTaskById = (params) => taskRepo.getTaskById(params);
-const updateTask = (params, data) => taskRepo.updateTask(params, data);
-const deleteTask = (params) => taskRepo.deleteTask(params);
+const addBoardTask = (data) => {
+  const task = new Task({
+    title: data.body.title,
+    order: data.body.order,
+    description: data.body.description,
+    userId: data.body.userId,
+    boardId: data.params.boardId,
+    columnId: data.body.columnId,
+  });
+  taskRepo.addBoardTask(task);
+  return task;
+};
+const getTaskById = (params) =>
+  taskRepo.getTaskById({
+    board: params.boardId,
+    task: params.taskId,
+  });
+const updateTask = (body, options) => {
+  const update = {
+    title: body.title,
+    order: body.order,
+    description: body.description,
+    userId: body.userId,
+    boardId: body.boardId,
+    columnId: body.columnId,
+  };
+  const params = {
+    board: options.boardId,
+    task: options.taskId,
+  };
+  taskRepo.updateTask(params, update);
+  return getTaskById(params);
+};
+const deleteTask = (params) =>
+  taskRepo.deleteTask({
+    board: params.boardId,
+    task: params.taskId,
+  });
 
 module.exports = {
   getBoardTasks,
