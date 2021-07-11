f5053ea277470c6fd969ca5ac39112609b2014cb
petrgali <opelliek@gmail.com>
2021-07-11 23:58:08 +0600

    fail: fastify logger fail

--- src/filters/exceptions.filter.ts
+++ src/filters/exceptions.filter.ts
@@ -4,29 +4,29 @@ import { BaseExceptionFilter } from '@nestjs/core';
 @Catch()
 export class ExceptionsLoggerFilter extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    const { ip, url, method } = host.getArgs()[1].req;
+    // const { ip, url, method } = host.getArgs()[1];
 
     console.log(
       'Exception thrown',
-      {
-        ip,
-        url,
-        method,
-      },
+      // {
+      //   ip,
+      //   url,
+      //   method,
+      // },
       exception,
     );
 
-    process.on('unhandledRejection', () => {
-      console.log(
-        'Rejection thrown',
-        {
-          ip,
-          url,
-          method,
-        },
-        exception,
-      );
-    });
+    // process.on('unhandledRejection', () => {
+    //   console.log(
+    //     'Rejection thrown',
+    //     {
+    //       ip,
+    //       url,
+    //       method,
+    //     },
+    //     exception,
+    //   );
+    // });
     super.catch(exception, host);
   }
 }
